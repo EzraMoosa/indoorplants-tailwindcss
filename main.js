4a1eeeaf4bcc21bd3850f6fcce5ec3d783fc608a
ezramoosa <ezrae18@gmail.com>
2025-01-03 01:05:38 +0200

    Add scroll in effect to review image and swiper.

--- main.js
+++ main.js
@@ -106,11 +106,11 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal('.home__data, .about__top, .popular__top')
+sr.reveal('.home__data, .about__top, .popular__top, .review__top, .review__swiper')
 sr.reveal('.home__image', {delay: 500, scale: 0.5})
 
 sr.reveal('.service__card, .popular__card', {interval: 100})
 
 sr.reveal('.about__leaf', {delay: 1000, origin: "right"})
 sr.reveal('.about__item__1-content, .about__item__2-img', {origin: "right"})
-sr.reveal('.about__item__2-content, .about__item__1-img', {origin: "left"})
+sr.reveal('.review__leaf', {delay: 1000, origin: "left"})

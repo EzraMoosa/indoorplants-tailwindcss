9a29765c1d5b3827b4cca9a8ab001873792c8e45
ezramoosa <ezrae18@gmail.com>
2025-01-03 00:18:33 +0200

    Only add scroll up button when scrolled down.

--- main.js
+++ main.js
@@ -15,6 +15,20 @@ navLink.forEach(link => {
     })
 })
 
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+
+    if(this.scrollY > 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+}
+
+window.addEventListener("scroll", scrollUp)
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     speed: 400,

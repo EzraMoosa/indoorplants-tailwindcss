0890f03459b8116c533121656616c7b223f003f7
ezramoosa <ezrae18@gmail.com>
2025-01-02 13:56:57 +0200

    Add burger menu toggle.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 /* Toggle button */
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".nav-link")
+const hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]")
+})
